008a3e2f1ef845bb48f43cf0d963a3eb2b0b9292
Galip Sen <galipshen@gmail.com>
2023-12-25 16:26:36 +0300

    counter created with redux

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View, StatusBar } from "react-native";
+import { Provider, useSelector, useDispatch } from "react-redux";
+import React from "react";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+//define an initial state
+const initialState = {
+  counter: 0,
+};
+//reducers obj update the state
+const counterSlice = createSlice({
+  name: "counterSlice",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.counter += 1;
+    },
+    decrement: (state) => {
+      state.counter -= 1;
+    },
+  },
+});
+
+// configureStore creates a redux store by calling intenally createStore  method
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
+/*  
+// Video source
+
+<Text>https://www.youtube.com/watch?v=cdxoHp-vZpY</Text>
+
+*/
+function Counter() {
+  // get state with useSelector
+  const counter = useSelector((state) => state.counter);
+  //update counter with dispatch
+  const dispatch = useDispatch();
+  return (
+    <>
+      <View>
+        <StatusBar />
+        <Text>Counter: {counter}</Text>
+        <Button
+          title="increment"
+          onPress={() => dispatch(counterSlice.actions.increment())}
+        />
+        <Button
+          title="decrement"
+          onPress={() => dispatch(counterSlice.actions.decrement())}
+        />
+      </View>
+    </>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <Counter />
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
